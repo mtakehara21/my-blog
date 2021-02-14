adc00703e0a0c06825c468367fb25f2606a1751a
Masahiro Takehara <azukibar21@gmail.com>
2021-02-14 09:55:58 +0900

    fix: ga setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "process.env.GOOGLE_ANALYTICS_ID",
+        trackingId: siteConfig.googleAnalyticsId,
         head: true,
       },
     },
